890af960711bc73a9bb36c019cb093acd73c9dac
Ethan Mathias <ethanmathias123@gmail.com>
2025-01-17 14:19:01 +0000

    background image modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,39 @@ import Payment from './pages/Payment';
 import "@radix-ui/themes/styles.css";
 import { Flex, Theme } from "@radix-ui/themes";
 import Result from './pages/Result';
-import background from './assets/background.jpeg'
-
+import background from './assets/background.png'
+import {React, Component} from "react"
 
 
 function App() {
 
+const myStyle = {
+    backgroundImage: `url(${background})`,
+    height: "100px",
+    marginTop: "-70px",
+    fontSize: "50px",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  }
 
   return (
-    <div style={{backgroundImage: `url(${background})`,
-                backgroundSize: "100% 100%"
-    }}>
-    <Theme
-      accentColor="indigo"
-      panelBackground="solid"
-      scaling="100%"
-      radius="medium"
-      appearance="dark"
-    >
-    <Flex justify="between">
-    <Payment />
-    <Result />
-    </Flex>
-    </Theme></div>
+    <>
+    <div
+      style={myStyle}
+    ></div>
+      <Theme
+        accentColor="indigo"
+        panelBackground="solid"
+        scaling="100%"
+        radius="medium"
+        appearance="dark"
+      >
+        <Flex justify="between">
+          <Payment />
+          <Result />
+        </Flex>
+      </Theme>
+    </>
   );
 }
 
